1d0f95038207f9e740c0560ce001195977c0ae72
Jack Weiss <jack.weiss528@gmail.com>
2018-11-07 13:14:29 -0500

    add

---
+++ function.js
@@ -0,0 +1,14 @@
+function HelloConsole(){
+  console.log("Hello, World!");
+}
+function popup(){
+  alert("Hello, World!");
+}
+function browser(){
+  var p = document.getElementById("hello");
+  p.innerHTML = "Hello, World!";
+}
+function stranger(){
+  var answer = prompt("Please Enter Your Name");
+  alert("Hello, " + answer + "!");
+}
